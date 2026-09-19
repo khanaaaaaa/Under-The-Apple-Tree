82c28791b4669e980b7657f63a36660abd00275c
khanaaaaaa <khanalaastha88@gmail.com>
2026-09-19 21:43:16 +0545

    Worked on making the actual layout - tried live sharing couldn't figure it out

--- src/main.js
+++ src/main.js
@@ -20,3 +20,53 @@ const config = {
         update: update
     }
 };
+
+const game = new Phaser.Game(config);
+
+function preload() {
+    // later
+}
+
+function create() {
+    this.add.rectangle(
+        480,
+        270,
+        960,
+        540,
+        0x8fcf7a
+    );
+
+    this.add.rectangle(
+        480,
+        430,
+        960,
+        100,
+        0xc99a63
+    );
+
+    this.add.rectangle(
+        180,
+        270,
+        90,
+        320,
+        0xc99a63
+    );
+
+    createFarmPlot(this, 390, 170);
+    createFarmPlot(this, 560, 170);
+
+    createFarmPlot(this, 390, 300);
+    createFarmPlot(this, 560, 300);
+
+    this.addeclipse(
+        770,
+        130,
+        280,
+        140,
+        0x62b6d4
+    )
+};
+
+function update() {
+    // game logic later
+}
